9dd088a63797229568c3128505f8ca571b3c1100
Justin Huynh <j.alexhuynh@gmail.com>
2018-07-08 10:15:36 -0700

    Establish folder structure. Add node dependencies. Add root routes.

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+	app 	= express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(res, res) {
+	res.render("index");
+});
+
+app.get("/about", function(res, res) {
+	res.render("about");
+});
+
+app.get("/portfolio", function(res, res) {
+	res.render("portfolio");
+});
+
+app.get("/contact", function(res, res) {
+	res.render("contact");
+});
+
+app.listen(3000, function() {
+	console.log("Server online at localhost:3000");
+});
