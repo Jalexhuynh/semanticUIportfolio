95b18349010f7f755b3e5048f7c51fc2e2963256
Justin Huynh <j.alexhuynh@gmail.com>
2018-07-08 15:18:27 -0700

    Create script.js file. Add contact form skeleton.

---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(main);
+
+function main() {
+	$('.ui.top.sidebar').sidebar({
+		dimPage: true,
+		transition: 'overlay',
+		exclusive: false,
+		closable: true
+	});
+
+	$('.ui.top.sidebar')
+		.sidebar('attach events', '#right-sidebar-toggle', 'toggle');
+
+	$('.ui.form')
+	  .form({
+		fields: {
+		  firstName: {
+			identifier: 'first-name',
+			rules: [
+			  {
+				type   : 'empty',
+				prompt : 'Please enter your first name.'
+			  }
+			]
+		  },
+		  lastName: {
+			identifier: 'last-name',
+			rules: [
+			  {
+				type   : 'empty',
+				prompt : 'Please enter your last name.'
+			  }
+			]
+		  },
+		  email: {
+			identifier: 'email',
+			rules: [
+			  {
+				type   : 'empty',
+				prompt : 'Please enter your email address.'
+			  }
+			]
+		  },
+		  message: {
+			identifier: 'message',
+			rules: [
+			  {
+				type   : 'empty',
+				prompt : 'Please enter a message'
+			  }
+			]
+		  }
+		}
+	  })
+	;
+}
