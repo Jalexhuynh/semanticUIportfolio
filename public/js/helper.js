2c3159c183d1a8b0928b53abc1522c709527955c
Justin Huynh <j.alexhuynh@gmail.com>
2018-07-09 18:22:09 -0700

    Create JSON objects for about page content.

---
+++ public/js/helper.js
@@ -0,0 +1,16 @@
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolDegree = '<h3 class="ui header">%data% in ';
+var HTMLschoolMajor = '%data%</h3>';
+var HTMLschoolName = '<p>%data%' ;
+var HTMLschoolLocation = ', %data%';
+var HTMLschoolDates = ', %data%</p>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkTitle = '<h3 class="ui header">%data%</h3>';
+var HTMLworkEmployer = '<p>%data% ';
+var HTMLworkLocation = '%data% ';
+var HTMLworkDates = '%data% ';
+var HTMLworkDescription = '<p>%data%</p><br>';
+
+var HTMLskillsStart = '<div class="ui list"><ul class="skills-entry"></ul></div>';
+var HTMLskillsItem = '<li class="item">%data%</li>';
--- public/js/script.js
+++ public/js/script.js
@@ -65,4 +65,104 @@ function main() {
 	$("#test1").progress();
 	$("#test2").progress();
 	$("#test3").progress();
+	displayEducation();
+	displayWork();
 }
+
+var education = {
+	"schools":
+	[
+		{
+			"name": "University of California, Los Angeles",
+			"degree": "Bachelors of Science",
+			"major": "Microbiology, Immunology, and Molecular Genetics",
+			"location": "CA",
+			"years": "2011 - 2015"
+		}
+	]
+};
+
+var work = {
+	"jobs":
+	[
+		{
+			"title": "Research Technician I",
+			"employer": "La Jolla Institute for Allergy & Immunology",
+			"location": "San Diego, CA",
+			"dates": "DEC 2015 - JAN 2017",
+			"description": "Conducted research on human immune responses to Tuberculosis (TB) though ELISPOT assays" + 
+						   " analysis of Mucosal-Associate Invariant T (MAIT) cells by flow cytometry and in-vitro" + 
+						   " assays, as well as routinely isolating peripheral blood mononuclear cells (PBMCs) from" +
+						   " post-active TB or latent TB blood samples. Also assisted in maintaining lab inventory" +
+						   " of supplies, plasma/cell samples, and independently coordinating experiments with the PI."
+		},
+		{
+			"title": "Research Technician I",
+			"employer": "USC Keck School of Medicine",
+			"location": "Los Angeles, CA",
+			"dates": "AUG 2015 - OCT 2015",
+			"description": "Processed HIV samples through RNA extractions of infected Plasma, PCR runs, and gel" + 
+						   " electrophoresis. Assisted in 454 sequencing, and led the design of PCR primers using" + 
+						   " python code to determine the best primer pair to use for amplification of pol and env" +
+						   " regions of HIV samples in a novel research project. "
+		},
+		{
+			"title": "Barista",
+			"employer": "UCLA Bruin Cafe",
+			"location": "Los Angeles, CA",
+			"dates": "SEP 2012 - JUN 2014",
+			"description": "Managed and cooperated with team members to perform various assigned tasks, as well as" +
+						   " handle independent duties without requiring supervision. Also took charge of training" + 
+						   " new employees in performing their assigned positions. "
+		}
+	]
+};
+
+function displayEducation() {
+	for (school in education.schools) {
+		var formattedSchoolName 		= HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formattedSchoolDegree 		= HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedSchoolMajor 		= HTMLschoolMajor.replace("%data%", education.schools[school].major);
+		    formattedSchoolFullDegree 	= formattedSchoolDegree + formattedSchoolMajor;
+		var formattedSchoolLocation 	= HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formattedSchoolYears 		= HTMLschoolDates.replace("%data%", education.schools[school].years);
+		var formattedFullEducation		= formattedSchoolName + formattedSchoolLocation + formattedSchoolYears;
+
+		$("#education").append(HTMLschoolStart);
+		$(".education-entry:last").append(formattedSchoolFullDegree);
+		$(".education-entry:last").append(formattedFullEducation);
+	};
+};
+
+function displayWork() {
+	for (job in work.jobs) {
+		var formattedWorkTitle 			= HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedWorkEmployer 		= HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedWorkDates 			= HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedWorkLocation 		= HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedFullEmployer		= formattedWorkEmployer + formattedWorkDates + formattedWorkLocation;
+		var formattedWorkDescription 	= HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+		$("#work-experience").append(HTMLworkStart);
+		$(".work-entry:last").append(formattedWorkTitle);
+		$(".work-entry:last").append(formattedFullEmployer);
+		$(".work-entry:last").append(formattedWorkDescription);		
+
+	};
+};
+
+var skills = {
+	"skills":
+	["Programming", "Web Design", "Microbiology", "Graphic Design",
+				"Javascript", "HTML", "CSS", "Python", "Adobe Photoshop"]
+};
+
+var displaySkills = function() {
+	$("#skills").append(HTMLskillsStart);
+	for (skill in skills.skills) {
+		var formattedSkill = HTMLskillsItem.replace("%data%", skills.skills[skill]);
+		$(".skills-entry:last").append(formattedSkill);
+	};
+};
+
+displaySkills();
